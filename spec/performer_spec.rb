678434df69802736b7951baa262df251af3d2dbc
David Malin <david.malin@gmail.com>
2011-12-30 21:27:05 -0500

    dollars for railscasts and vcd rocks

--- spec/performer_spec.rb
+++ spec/performer_spec.rb
@@ -18,18 +18,15 @@ describe "Ticketevolution::Perfomer" do
       
    
     it "should wih JSON for the respectiveb object through the find call" do
-
-      cassette = YAML.load(File.open('spec/webmocks/performers_get_200.yml'))
-      stub_request(:get, "https://api.sandbox.ticketevolution.com/performers/9?").with(:headers => {'Accept'=>'application/vnd.ticketevolution.api+json; version=8', 'X-Signature'=>'HtLlrlGmNWMEyRPdSCaRvnW3+YB1WnvMJ6q4/liCr9A=', 'X-Token'=>'958acdf7da43b57ac93b17ff26eabf45'}).to_return(:status => 200, :body => cassette["body_str"], :headers => cassette["header_str"])
-      path = "#{@http_base}.ticketevolution.com/performers/9?"
-      performer = Ticketevolution::Performer.find(9)
-      
-      performer.name.should == ("Dipset")
-      performer.updated_at.should == ("2011-02-05T09:49:26Z")
-      performer.category.should == (nil)
-      performer.venue.should == (nil)
-      performer.upcoming_events.should == ({"last"=>nil, "first"=>nil})
-      
+      VCR.use_cassette "perfomer/#find/200" do
+        path = "#{@http_base}.ticketevolution.com/performers/9?"
+        performer = Ticketevolution::Performer.find(3219)      
+        performer.name.should == ("Dipset")
+        performer.updated_at.should == ("2011-02-05T09:49:26Z")
+        performer.category.should == (nil)
+        performer.venue.should == (nil)
+        performer.upcoming_events.should == ({"last"=>nil, "first"=>nil})
+      end
 
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,13 @@ require 'rubygems'
 require 'tempfile'
 require 'rspec'
 require 'rake'
-require 'webmock/rspec'
 require 'yaml'
+require 'vcr'
 
-
+VCR.config do |c|
+  c.cassette_library_dir  = File.join(File.dirname(File.expand_path(__FILE__)), "vcr")
+  c.stub_with :webmock
+end
 
 require File.join(File.dirname(File.expand_path(__FILE__)), "..", "lib", "ticketevolution")
 
