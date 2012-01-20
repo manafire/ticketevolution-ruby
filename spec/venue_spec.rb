592d4032daf3dbf728d8d06bbcaa3b0396a4a47b
Dan Matthews <dan@ticketevolution.com>
2012-01-20 12:33:03 -0500

    Fixed venue spec that lists venue based on name to work with WebMock's limitation of not properly supporting URI's with a space.

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -10,7 +10,7 @@ describe "TicketEvolution::Venue" do
         config.mode     = :sandbox
       end
     end
-    
+
     it "should assemble the correct signature and the correct path to perform the get with" do
       VCR.use_cassette "venue/find_by_id" do
         response = TicketEvolution::Venue.find(896)
@@ -18,9 +18,9 @@ describe "TicketEvolution::Venue" do
         response.url.should             ==  "/venues/896"
         response.location               ==  "New York, NY"
       end
-    end        
+    end
   end
-  
+
   # There is an issue with VCR. EVERY time I run the specs again for some reason I get a message that http requests
   # are banned but only AFTER it has been recorded. New Tool To Me , sure what im doing wrong.
   # [DKM 2012.01.15]
@@ -33,19 +33,19 @@ describe "TicketEvolution::Venue" do
         config.mode     = :sandbox
       end
     end
-        
+
     it "should assemble the correct signature and the correct path to perform the get with" do
       VCR.use_cassette "venue/list_by_name" do
-        response = TicketEvolution::Venue.list(:name => "Allianz Arena")
+        response = TicketEvolution::Venue.list(:name => "Troubadour")
         response.first.class.should == TicketEvolution::Venue
         response.should.class       == Array
-        response.first.name.should  == "Allianz Arena"
-        response.first.url.should   == "/venues/6512"
+        response.first.name.should  == "Troubadour"
+        response.first.url.should   == "/venues/1606"
         response.length.should      == 1
       end
-    end        
+    end
   end
-  
+
   # describe "#find_by_performer" do
   #     before(:all) do
   #       TicketEvolution::configure do |config|
@@ -55,8 +55,8 @@ describe "TicketEvolution::Venue" do
   #         config.mode     = :sandbox
   #       end
   #     end
-  #     
-  #     
+  #
+  #
   #     it "should return a list of venues" do
   #       initial_events = []
   #       VCR.use_cassette "venue/find_performer_events" do
@@ -64,13 +64,13 @@ describe "TicketEvolution::Venue" do
   #         events_for_performer.class.should == Array
   #         events_for_performer.class.first.should == TicketEvolution::Event
   #       end
-  #       
+  #
   #     end
   #   end
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
 end
